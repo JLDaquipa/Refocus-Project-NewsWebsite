6030ca3440957d0d967e6c0f00b4e12cbb0d9510
JLDaquipa <JLPDfive5@gmail.com>
2023-03-22 08:54:52 +0800

    Updated semantic layout and made some design adjustments

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ renderCards();
 
 document.addEventListener('click',(e) => {
     if(e.target.dataset.newsId){ // When heart icon was clicked
-        handleLikeClick(e.target.dataset.newsId)
+        handleLikeClick(e.target.dataset.newsId);
     }
     else if(e.target.dataset.removeNewsId){ // When close card was clicked
         e.target.closest('#news-card').style.display = "none";
@@ -39,7 +39,7 @@ document.querySelectorAll('#news-card').forEach(card => { // Hover cards listene
     })
 })
 
-// Function that handles events where users click the heart icon on cards
+// Function that handles events where user click the heart icon on cards
 function handleLikeClick(newsId){ 
 
     const newsObject = newsData.find(news => news.id === parseInt(newsId));
@@ -83,47 +83,37 @@ function renderRead(){
         if(news.isRead){
             readIcon.style.display = "inline";
         }else{
-            readIcon.style.display = "none"
+            readIcon.style.display = "none";
         }
     })
 }
 
 // Renders all cards to DOM function
 function renderCards(){
-
     let feedHtml = "";
 
     newsData.forEach(news => {
-        
-        let isLikeClass = '';
-
-        if(news.isLiked){
-            isLikeClass = 'fa-solid';
-        }else{
-            isLikeClass = 'fa-regular';
-        }
-
         feedHtml += `
 <div class="news-card" id="news-card" data-news-id="${news.id}">
-    <i class="fa-regular fa-circle-xmark close-btn" id="close-btn-${news.id}" data-remove-news-id="${news.id}"></i>
-    <div class="image-box" id="image-box-${news.id}"><img class="image" src="images/${news.image}" alt="News Banner Image"></div>
-    <div class="card-content">
-        <i class="fa-regular fa-circle-check read-icon" id="read-icon-${news.id}" data-read-news-id="${news.id}"></i>
-        <h1 class="card-title">${news.title}</h1>
-        <div class="card-elem">
-            <div class="card-date">
-                <span id="card-date">${news.date}</span>
-            </div>
-            <div class="heart">
-                <i class="fa-sharp ${isLikeClass} fa-heart" id="like-icon-${news.id}" data-news-id="${news.id}"></i>
-                <span id="likes-count-${news.id}"></span>
+    <article>
+        <i class="fa-regular fa-circle-xmark close-btn" id="close-btn-${news.id}" data-remove-news-id="${news.id}"></i>
+        <div class="image-box" id="image-box-${news.id}"><img class="image" src="images/${news.image}" alt="News Banner Image"></div>
+        <div class="card-content">
+            <i class="fa-regular fa-circle-check read-icon" id="read-icon-${news.id}" data-read-news-id="${news.id}"></i>
+            <h1 class="card-title">${news.title}</h1>
+            <div class="card-elem">
+                <div class="card-date">
+                    <span id="card-date">${news.date}</span>
+                </div>
+                <div class="heart">
+                    <i class="fa-sharp fa-regular fa-heart" id="like-icon-${news.id}" data-news-id="${news.id}"></i>
+                    <span id="likes-count-${news.id}"></span>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-`;
-    })
-
+    </article>
+</div>`;
+})
     document.getElementById('news-container').innerHTML = feedHtml;
     renderRead();
     renderLikes();
